a94511d13452241c4330d5ca215780f7111bb6c6
binishkhan997-stack <binishkhan997@gmail.com>
2026-07-08 20:02:22 +0500

    Create index.php

---
+++ config/config/public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once "../config/config.php";
+
+require_once "../config/database.php";
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>
+
+<?php echo APP_NAME; ?>
+
+</title>
+
+</head>
+
+<body>
+
+<h1>
+
+Welcome To DailyMarket
+
+</h1>
+
+<p>
+
+Pakistan Online Marketplace
+
+</p>
+
+</body>
+
+</html>
